8dd732e6beab44b80ed20f7bec03b42d6b3d6ee0
kovacsrud <kovacs.rud@gmail.com>
2024-09-12 10:20:21 +0200

    Update index.js

--- alap_backend/index.js
+++ alap_backend/index.js
@@ -53,4 +53,45 @@ app.post('/termek',(req,res)=>{
     adatok.push(req.body);
 
     res.status(201).json({message:"Adat beillesztve!"})
-})
+});
+
+app.patch('/termek',(req,res)=>{
+    let adat=req.body;
+    let modositando=adatok.findIndex(x=>x.id==adat.id);
+    if(modositando>-1){
+        adatok[modositando]=adat;
+        res.status(200).json({message:"Adat módosítva!"});
+    } else {
+        res.status(404).json({message:"Az adat nem található!"});
+    }
+});
+
+app.delete('/termek/:id',(req,res)=>{
+    let id=req.params.id;
+    adatok=adatok.filter(x=>x.id!=id);
+    res.status(200).json({message:"A termék törölve!"});
+});
+
+app.get('/marka/:marka',(req,res)=>{
+    const {marka}=req.params;
+    let result=adatok.filter(x=>x.marka.toLowerCase()==marka.toLowerCase());
+    if(result.length>0){
+        res.status(200).json(result);
+    } else {
+        res.status(404).json({message:"Nincs ilyen márka!"})
+    }
+
+});
+
+app.get('/termeknev/:termeknev',(req,res)=>{
+    const {termeknev}=req.params;
+    let result=adatok.filter(x=>x.termeknev.toLowerCase()==termeknev.toLowerCase());
+
+    if(result.length>0){
+        return res.status(200).json(result);
+    }
+
+    res.status(404).json({message:"Nincs ilyen nevű termék!"});
+
+
+});
