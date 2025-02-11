4a2d112188e0ca3745ddab0d901fbcecd9776e34
kovacsrud <kovacs.rud@gmail.com>
2025-02-11 13:33:46 +0100

    Create KutyaController.cs

---
+++ KutyaAPI/KutyaAPI/Controllers/KutyaController.cs
@@ -0,0 +1,29 @@
+﻿using KutyaAPI.mvc.model;
+using KutyaAPI.mvc.modelview;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KutyaAPI.Controllers
+{
+    [ApiController]
+    public class KutyaController : Controller
+    {
+        private KutyaModelData kutyaData = new KutyaModelData();
+
+        [HttpGet("/api/kutyak")]
+        public IActionResult GetKutyak()
+        {
+            try
+            {
+                return Ok(
+                   
+                   kutyaData.context.Set<Kutya>().Select(i => new{ i.Id,i.Fajta.Nev,i.Fajtaid,i.Nev.Kutyanev,i.Nevid,i.Eletkor,i.Utolsoell }).ToList()
+                
+                );
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
